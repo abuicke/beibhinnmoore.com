c175fa943dec0cf2d83cd0e1739f2cdf406a8303
abuicke <abuicke@treemetrics.com>
2018-02-04 20:24:33 +0000

    fixed image squashing as far as third row
    fixed image squashing as far as third row

--- gallery.js
+++ gallery.js
@@ -11,20 +11,51 @@ $(document).ready(function() {
                 .appendTo('#gallery');
         }
 
-        $('<a href="images/' + i + '.jpg" target="_blank"><img src="images/' + i + '.jpg"/></a>')
+        $('<a href="images/' + i + '.jpg" target="_blank"><img id="image-' + i + '" src="images/' + i + '.jpg"/></a>')
             .appendTo('#row-' + row);
-
-        // Wait for images to load.
-        $(window).on("load", function() {
-            var rowDiv = $('#row-1');
-            console.log("1: row " + 1 + " height " + rowDiv.height());
-        })
     }
 
-//    // Wait for images to load.
-//    $(window).on("load", function() {
-//        var rowDiv = $('#row-1');
-//        console.log("2: row " + 1 + " height " + rowDiv.height());
-//    })
+    // Wait for images to load.
+    //    $(window).on("load", function() {
+    setTimeout(function() {
+        adjustImagePositions();
+    }, 100);
 
 });
+
+function adjustImagePositions() {
+
+    for (topRow = 2, bottomRow = 3; topRow <= 4; topRow++, bottomRow++) {
+        for (column = 1; column <= 3; column++) {
+            
+            console.log("top image " + getImageId(column, topRow));
+            console.log("bottom image " + getImageId(column, bottomRow));
+            console.log("====================================================");
+            
+            var topImage = getImage(column, topRow);
+            var bottomImage = getImage(column, bottomRow + 1);
+
+            var imgHeight = topImage.clientHeight;
+            var rowHeight = getRowHeight(2);
+
+            if(imgHeight < rowHeight) {
+                var delta = rowHeight - imgHeight;
+                $("#" + getImageId(column, bottomRow)).css({bottom: delta});
+            }
+        }
+    } 
+}
+
+function getImageId(column, row) {
+    return 'image-' + (((row - 1) * 3) + column);
+}
+
+function getImage(column, row) {
+    var imageId = getImageId(column, row);
+    var img = document.getElementById(imageId); 
+    return img;
+}
+
+function getRowHeight(row) {
+    return $('#row-' + row).height();
+}
