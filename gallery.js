6a6a3c666ba1dd3d225d0d7177147db2b3df1712
abuicke <abuicke@treemetrics.com>
2018-02-07 23:26:09 +0000

    implemented lazy loading
    implemented lazy loading

--- gallery.js
+++ gallery.js
@@ -1,12 +1,22 @@
 $(document).ready(function() {
 
-    $(window).on("load", function() {
+    var imagesLoaded = 0;
 
-        $('.grid').masonry({
-            itemSelector: '.grid-item',
-            columnWidth: 0
-        }); 
+    $('.grid-item').on('load', function() {
+        // Run masonry after each row is loaded.
+        if((imagesLoaded % 3) === 0) {
+            $('.grid').masonry({
+                itemSelector: '.grid-item',
+                columnWidth: 0
+            });
+        }
 
+        imagesLoaded++;
     });
 
+    // Load the images in seqeunce so that the
+    // most offscreen images are loaded last.
+    for(i = 1; i <= 41; i++){
+        $('#image-' + i).attr("src", "images/" + i + ".jpg");
+    };
 });
