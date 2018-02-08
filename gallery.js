2a13a5c30b439bc3df74fe875883a7fedd39c6a5
abuicke <abuicke@treemetrics.com>
2018-02-08 17:59:24 +0000

    recursive image loading
    recursive image loading

--- gallery.js
+++ gallery.js
@@ -1,22 +1,22 @@
 $(document).ready(function() {
-
-    var imagesLoaded = 0;
-
-    $('.grid-item').on('load', function() {
-        // Run masonry after each row is loaded.
-        if((imagesLoaded % 3) === 0) {
-            $('.grid').masonry({
-                itemSelector: '.grid-item',
-                columnWidth: 0
-            });
-        }
-
-        imagesLoaded++;
-    });
-
-    // Load the images in seqeunce so that the
-    // most offscreen images are loaded last.
-    for(i = 1; i <= 41; i++){
-        $('#image-' + i).attr("src", "images/" + i + ".jpg");
-    };
+    loadImages(1, 41);
 });
+
+function loadImages(firstImage, lastImage) {
+    loadImage(firstImage, func = function() {
+        $('.grid').masonry({
+            itemSelector: '.grid-item',
+            columnWidth: 0
+        });
+
+        if(firstImage < lastImage){
+            loadImage(++firstImage, func);
+        }
+    });
+}
+
+function loadImage(index, f) {
+    $('#image-' + index)
+        .attr("src", "images/" + index + ".jpg")
+        .on('load', f);
+}
