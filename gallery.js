42fe83d583819dbf7f545fd30cf0fdf184b1c19e
abuicke <abuicke@treemetrics.com>
2018-02-08 20:10:44 +0000

    implemented blurry to sharp lazy loading
    implemented blurry to sharp lazy loading

--- gallery.js
+++ gallery.js
@@ -1,8 +1,17 @@
 $(document).ready(function() {
-    loadImages(1, 41);
+    
+    $(window).on('load', function() {
+        $('.grid').masonry({
+            itemSelector: '.grid-item',
+            columnWidth: 0
+        });
+        
+        loadImages(1, 41);
+    });
 });
 
 function loadImages(firstImage, lastImage) {
+
     loadImage(firstImage, func = function() {
         $('.grid').masonry({
             itemSelector: '.grid-item',
@@ -16,7 +25,15 @@ function loadImages(firstImage, lastImage) {
 }
 
 function loadImage(index, f) {
+    setTimeout(function() {
+        $('#image-' + index)
+            .attr("src", "images/" + index + ".jpg")
+            .on('load', f); 
+    }, 2000);
+}
+
+function loadPlaceholder(index, f) {
     $('#image-' + index)
-        .attr("src", "images/" + index + ".jpg")
+        .attr("src", "images/placeholders/" + index + ".jpg")
         .on('load', f);
 }
