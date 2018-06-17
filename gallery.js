7313c90944aff31c55c626194488010c17847308
abuicke <mosesjayjayjay@gmail.com>
2018-06-17 14:56:17 +0100

    moved document ready logic to head of page
    moved document ready logic to head of page

--- gallery.js
+++ gallery.js
@@ -1,17 +1,3 @@
-$(document).ready(function() {
-    console.log("document ready");
-    $(window).on('load', function() {
-        console.log("window loaded");
-        $('.grid').one('layoutComplete', function() {
-            console.log("layout complete");
-            loadImages(1, 41);
-        }).masonry({
-            itemSelector: '.grid-item',
-            columnWidth: 0
-        });
-    });
-});
-
 function loadImages(firstImage, lastImage) {
     loadImage(firstImage, func = function() {
         $('.grid').masonry({
